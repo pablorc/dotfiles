8cbb7c8aa39c2fe18b36fa81f70f5a42eebcae61
Pablo Rivera Conde <pabloriveraconde@gmail.com>
2023-10-17 16:42:00 +0200

    Better todayist cli

--- stow/starship/.pbl/bin/todayist.sh
+++ stow/starship/.pbl/bin/todayist.sh
@@ -1,23 +1,70 @@
 #!/usr/bin/env sh
 
-url="http://localhost:3333"
-json="$(curl $url/api/tasks 2>/dev/null)"
+printList() {
+  length=$(echo $json | jq -c $(echo $2 | tr [] "  ") | jq length)
+
+  if [ $length != "0" ]; then
+
+    echo -e "\n---\e[1m $1 \e[0m---"
+
+    echo $json | jq -c $2  | while read task; 
+    do 
+      pp $task
+    done
+  fi
+}
+
+pp(){
+  task=$*
+
+  completed="$(echo $task | jq '.Completed')"
+  str_start="[ ] "
+  str_end=""
+
+  if [ ! -z $completed ] && [ $completed = true ]; then
+    str_start="[x] \e[9m"
+    str_end="\e[0m"
+  fi
+
+  echo -e "$str_start$(echo $task | jq -r '(.ID|tostring) + ": " + .Title')$str_end"
+}
+
+url="https://todayist.onrender.com"
+token="Authorization: Basic $TODAYIST_API_KEY"
 
 case "$1" in
 "prompt")
+  json="$(curl -H "$token" "$url/api/tasks" 2>/dev/null)"
+
   overdues="$(echo $json | jq '.OverduedTasks | length')"
   today="$(echo $json | jq '.TodayTasks | length')"
   
-  echo "$overdues over, $today today";;
+  echo "$overdues over, $today today"
+  ;;
+
 "tasks")
-	echo "$(echo $json | jq -r '.TodayTasks[] | (.ID|tostring) + ": " + .Title')";;
+  json="$(curl -H "$token" "$url/api/tasks" 2>/dev/null)"
+  
+  printList 'Today' '.TodayTasks[]' 
+  printList 'Overdue' '.OverduedTasks[]' 
+  printList 'Future' '.UpcomingTasks[]' 
+
+  ;;
 "close")
-	id=$2
-	res=$(curl "$url/api/tasks/done?id=$id" 2>/dev/null)
-	if [ $res='"ok"' ]
-	then
-		echo "Done"
-	else
-		echo "Error"
-	fi
+  id=$2
+  res=$(curl  -H "$token" "$url/api/tasks/done?id=$id" 2>/dev/null)
+
+  if [ $res='"ok"' ]
+  then
+    echo "Done"
+  else
+    echo "Error"
+  fi
+
+  ;;
+"create")
+  name=$(echo $2 | sed 's/ /%20/g')
+  res=$(curl -H "$token" "$url/api/tasks/create?name=$name" 2>/dev/null)
+  echo $res
 esac
+
